7219804d465d7d0edc2fb88f872b03c07be88932
unknown <mohamadmowfakhlal@gmail.com>
2022-04-18 00:21:53 +0300

    add sign up activity

---
+++ app/src/main/java/com/example/shareshipment/ConfirmPersonalInfo.java
@@ -0,0 +1,14 @@
+package com.example.shareshipment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ConfirmPersonalInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confirm_personal_info);
+    }
+}
---
+++ app/src/main/java/com/example/shareshipment/UserName.java
@@ -0,0 +1,4 @@
+package com.example.shareshipment;
+
+public class UserName {
+}
