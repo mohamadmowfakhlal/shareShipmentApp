94ac64cd3a75757ff407da37b28cb32d300a2491
unknown <mohamadmowfakhlal@gmail.com>
2022-05-12 21:43:39 +0300

    add size and weight assign button to listview showing list of shipment

--- app/src/main/java/com/example/shareshipment/MyAdapter.java
+++ app/src/main/java/com/example/shareshipment/MyAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.TextView;
 
 import org.json.JSONException;
@@ -14,7 +15,8 @@ import java.util.ArrayList;
 public class MyAdapter extends BaseAdapter {
    private Context context;
    private ArrayList<JSONObject> arrayList;
-   private TextView serialNum, name, contactNum;
+   private TextView shipmentId, shipmentType, shipmentFee,shipmentSize,shipmentWeight;
+   private Button assign;
    public MyAdapter(Context context, ArrayList<JSONObject> arrayList) {
       this.context = context;
       this.arrayList = arrayList;
@@ -34,13 +36,20 @@ public class MyAdapter extends BaseAdapter {
    @Override
    public View getView(int position, View convertView, ViewGroup parent) {
       convertView = LayoutInflater.from(context).inflate(R.layout.row, parent, false);
-      serialNum = convertView.findViewById(R.id.serailNumber);
-      name = convertView.findViewById(R.id.studentName);
-      contactNum = convertView.findViewById(R.id.mobileNum);
+      shipmentId = convertView.findViewById(R.id.shipmentId);
+      shipmentType = convertView.findViewById(R.id.ShipmentType);
+      shipmentFee = convertView.findViewById(R.id.ShipmentFee);
+      shipmentSize = convertView.findViewById(R.id.shipmentSize);
+      shipmentWeight = convertView.findViewById(R.id.shipmentWeight);
+      assign = convertView.findViewById(R.id.assign);
       try {
-      serialNum.setText(" " + arrayList.get(position).getInt("fee"));
-      name.setText(arrayList.get(position).getString("shipmentType"));
-      contactNum.setText(arrayList.get(position).getString("shipmentType"));
+      shipmentId.setText(" " + arrayList.get(position).getInt("shipmentId"));
+      shipmentType.setText(arrayList.get(position).getString("shipmentType"));
+      shipmentFee.setText(arrayList.get(position).getString("fee"));
+      if(arrayList.get(position).getString("size") != null)
+         shipmentSize.setText(arrayList.get(position).getString("size"));
+      if(arrayList.get(position).getString("weight") != null)
+         shipmentWeight.setText(arrayList.get(position).getString("weight"));
 
       } catch (JSONException e) {
          e.printStackTrace();
