052e35057fe8baa523d689c711fa898a4cb2dc7b
unknown <mohamadmowfakhlal@gmail.com>
2022-05-13 19:56:49 +0300

    add sign in activity

--- app/src/main/java/com/example/shareshipment/SignIn.java
+++ app/src/main/java/com/example/shareshipment/SignIn.java
@@ -6,22 +6,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class WelcomeActivity extends AppCompatActivity {
+public class SignIn extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_welcome);
-    }
-    public void signUp(View view) {
-        //ToDo call to rest api to send a sms to verify the phone number is a correct one
-        Intent intent = new Intent(this, SignUp.class);
-        startActivity(intent);
+        setContentView(R.layout.activity_sign_in);
     }
 
     public void signIn(View view){
         //ToDo API verification for phone number and password
-        Intent intent = new Intent(this, SignIn.class);
+        Intent intent = new Intent(this, MainFunctionality.class);
         startActivity(intent);
     }
 }
