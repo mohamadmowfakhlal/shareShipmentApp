393ca1e02a02207796adbe5d5aff0a36e144efa0
unknown <mohamadmowfakhlal@gmail.com>
2022-05-18 12:45:05 +0300

    check that the two password are identical

--- app/src/main/java/com/example/shareshipment/Password.java
+++ app/src/main/java/com/example/shareshipment/Password.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 
@@ -13,21 +14,30 @@ import org.json.JSONObject;
 
 public class Password extends AppCompatActivity {
     EditText password;
+    EditText repeatedPassword;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_password);
     }
     public void confirm(View view) throws JSONException {
-        password = findViewById(R.id.editTextTextPassword);
-        String pass = password.getText().toString();
-        JSONObject js = new JSONObject();
-        //phoneNumber should be taken from sign up information
-        String phoneNumber = ((MyApplication) this.getApplication()).getPhoneNumber();
-        js.put("phoneNumber",phoneNumber);
-        js.put("active","true");
-        js.put("password",pass);
-        String resource = "/users/";
-        CommonParams.JSONRequestWithoutResponse(js,resource, Request.Method.PUT,getApplicationContext(),MainFunctionality.class);
+        password = findViewById(R.id.password);
+        String passwordValue = password.getText().toString();
+        repeatedPassword = findViewById(R.id.repeatedPasswordValue);
+        String repeatedPasswordValue = repeatedPassword.getText().toString();
+        if(!passwordValue.equals(repeatedPasswordValue) ){
+            Toast.makeText(getApplicationContext(),
+                    "The password is not identical!",
+                    Toast.LENGTH_LONG).show();
+        }else{
+            JSONObject js = new JSONObject();
+            //phoneNumber should be taken from sign up information
+            String phoneNumber = ((MyApplication) this.getApplication()).getPhoneNumber();
+            js.put("phoneNumber",phoneNumber);
+            js.put("active","true");
+            js.put("password",passwordValue);
+            String resource = "/users/";
+            CommonParams.JSONRequestWithoutResponse(js,resource, Request.Method.PUT,getApplicationContext(),MainFunctionality.class);
+        }
     }
 }
