cae1f39f2524687af183da3b69f59424d3879f85
unknown <mohamadmowfakhlal@gmail.com>
2022-06-09 16:48:23 +0300

    change the shape of showed shipments

--- app/src/main/java/com/example/shareshipment/MainActivity.java
+++ app/src/main/java/com/example/shareshipment/MainActivity.java
@@ -12,7 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
     /** Called when the user taps the Send button */
     public void startApplication(View view) {
